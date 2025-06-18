6b00765e67fcd93ddc3157521dd42b400b374151
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-06-17 21:32:05 -0400

    Gun run 2

--- rifle_animator.gd
+++ rifle_animator.gd
@@ -6,9 +6,21 @@ const SHOOT_ROT = Vector3(deg_to_rad(5.0), deg_to_rad(173.8), 0.0)
 const RUN_POS = Vector3(-0.64, -0.63, -1.21)
 const RUN_ROT = Vector3(deg_to_rad(17.4), deg_to_rad(229.6), deg_to_rad(-23.0))
 
+var run_fac = 0.0;
+
 func _process(delta: float) -> void:
 	
-	var move := Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down").normalized().length()
+	var run_amount := Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down").normalized().length()
 	
-	position = lerp(SHOOT_POS, RUN_POS + Vector3(cos(Time.get_ticks_msec() * 0.01) * 0.03, abs(sin(Time.get_ticks_msec() * 0.01) * 0.03), 0.0), move)
-	rotation = lerp(SHOOT_ROT, RUN_ROT, move)
+	run_fac = lerp(run_fac, run_amount, delta * 20);
+	
+	position = lerp(
+		SHOOT_POS,
+		RUN_POS + Vector3(
+			cos(Time.get_ticks_msec() * 0.01) * 0.03,
+			abs(sin(Time.get_ticks_msec() * 0.01) * 0.03),
+			0.0
+		),
+		run_fac
+	)
+	rotation = lerp(SHOOT_ROT, RUN_ROT, run_fac)
