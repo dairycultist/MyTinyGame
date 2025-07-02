dff80c380f4cdec85aa9755dbfe14cb5322d41c1
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-07-01 20:46:41 -0400

    Update movement

--- player/player_controller.gd
+++ player/player_controller.gd
@@ -7,7 +7,9 @@ extends CharacterBody3D
 
 @export_group("Movement")
 @export var drag := 8
-@export var accel := 50
+@export var max_speed := 8
+@export var ground_accel := 50
+@export var air_accel := 30
 
 @export_group("Gun")
 @export var firerate := 8
@@ -85,17 +87,24 @@ func _process(delta: float) -> void:
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	
 	if is_on_floor():
+		
+		if direction:
+			velocity.x = lerp(velocity.x, max_speed * direction.x, ground_accel * delta)
+			velocity.z = lerp(velocity.z, max_speed * direction.z, ground_accel * delta)
+		
 		if Input.is_action_pressed("jump"):
 			velocity.y = 8
+		
+		velocity.x = lerp(velocity.x, 0.0, delta * drag)
+		velocity.z = lerp(velocity.z, 0.0, delta * drag)
+		
 	else:
+		
+		if direction:
+			velocity.x = lerp(velocity.x, max_speed * direction.x, air_accel * delta)
+			velocity.z = lerp(velocity.z, max_speed * direction.z, air_accel * delta)
+		
 		velocity.y -= 30 * delta
-	
-	if direction:
-		velocity.x += direction.x * accel * delta
-		velocity.z += direction.z * accel * delta
-	
-	velocity.x = lerp(velocity.x, 0.0, delta * drag)
-	velocity.z = lerp(velocity.z, 0.0, delta * drag)
 	
 	move_and_slide()
 
